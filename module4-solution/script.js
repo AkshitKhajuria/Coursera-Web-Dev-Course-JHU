8483c4222398c77fd763c7f2447b542162f90c77
Akshit Khajuria <khajurination@gmail.com>
2020-09-20 17:11:57 +0530

    IIFE

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -59,11 +59,11 @@ WARNING!!! WARNING!!!
     // byeSpeaker.xxxx
 
     // helloSpeaker.xxxx
-
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-
-names.forEach( name => {
-  const f_letter = name.charAt(0).toLowerCase();
-  if (f_letter!=='j'){ window.helloSpeaker.speak(name); }
-  else { window.byeSpeaker.speak(name); }
-})
+(()=>{
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  names.forEach( name => {
+    const f_letter = name.charAt(0).toLowerCase();
+    if (f_letter!=='j'){ window.helloSpeaker.speak(name); }
+    else { window.byeSpeaker.speak(name); }
+  })
+})()
