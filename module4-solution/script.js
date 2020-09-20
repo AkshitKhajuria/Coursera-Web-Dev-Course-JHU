3286e3ebb22166d66d0eccaa6b2aeaf30dcfbd7f
Akshit Khajuria <khajurination@gmail.com>
2020-09-20 16:51:56 +0530

    completed module 4

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -34,14 +34,12 @@ WARNING!!! WARNING!!!
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-
 // STEP 10:
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using the 'speak' method or either helloSpeaker's or byeSpeaker's
 // 'speak' method.
 // See Lecture 50, part 1
-for (/* fill in parts of the 'for' loop to loop over names array */) {
+/* fill in parts of the 'for' loop to loop over names array */ 
 
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
@@ -57,9 +55,15 @@ for (/* fill in parts of the 'for' loop to loop over names array */) {
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (/* fill in condition here */) {
+/* fill in condition here */
     // byeSpeaker.xxxx
-  } else {
+
     // helloSpeaker.xxxx
-  }
-}
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+names.forEach( name => {
+  const f_letter = name.charAt(0).toLowerCase();
+  if (f_letter!=='j'){ window.helloSpeaker.speak(name); }
+  else { window.byeSpeaker.speak(name); }
+})
